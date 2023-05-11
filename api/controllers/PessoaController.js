92d94bb2912704d8e1a40999b25283e4f8b5d61b
lealvesac <lealvesac@gmail.com>
2023-05-11 17:08:39 -0300

    update

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -2,6 +2,7 @@ const { where } = require("sequelize");
 const database = require("../models");
 
 class PessoaController {
+  // Pesquisar todos os cadastros.
   static async pegaTodasAsPessoas(req, res) {
     try {
       const todasAsPessoas = await database.Pessoas.findAll();
@@ -11,15 +12,65 @@ class PessoaController {
     }
   }
 
+  // Pesquisar cadastro por id.
   static async pegaUmaPessoa(req, res) {
     const { id } = req.params;
     try {
       const umaPessoa = await database.Pessoas.findOne({
         where: {
-          id: Number(id)
+          id: Number(id),
         },
       });
       return res.status(200).json(umaPessoa);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  // Fazer um novo cadastro.
+  static async criaPessoa(req, res) {
+    const novaPessoa = req.body;
+    try {
+      const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+      return res.status(200).json(novaPessoaCriada);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  // Atualização de cadastro
+  static async atualizaPessoa(req, res) {
+    const { id } = req.params;
+    const novasInfos = req.body;
+    try {
+      await database.Pessoas.update(novasInfos, {
+        where: {
+          id: Number(id),
+        },
+      });
+
+      const pessoaAtualizada = await database.Pessoas.findOne({
+        where: {
+          id: Number(id),
+        }
+      });
+
+      return res.status(200).json(pessoaAtualizada);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  // Deletar registro.
+  static async apagaPessoa(req, res) {
+    const {id} = req.params;
+    try {
+      await database.Pessoas.destroy( {
+        where: {
+          id: Number(id),
+        }
+      });
+      return res.status(200).json({mensagem: `ID ${id} deletado.`})
     } catch (error) {
       return res.status(500).json(error.message);
     }
--- api/routers/pessoasRoute.js
+++ api/routers/pessoasRoute.js
@@ -5,5 +5,8 @@ const router = Router();
 
 router.get("/pessoas", PessoaController.pegaTodasAsPessoas);
 router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
+router.post("/pessoas", PessoaController.criaPessoa);
+router.put("/pessoas/:id", PessoaController.atualizaPessoa);
+router.delete("/pessoas/:id", PessoaController.apagaPessoa);
 
 module.exports = router;
