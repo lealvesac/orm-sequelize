cace324693a0c2ab9cea8e74b17b245e91df1a92
lealvesac <lealvesac@gmail.com>
2023-05-11 09:37:33 -0300

    update

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const database = require("../models");
 
 class PessoaController {
@@ -5,6 +6,20 @@ class PessoaController {
     try {
       const todasAsPessoas = await database.Pessoas.findAll();
       return res.status(200).json(todasAsPessoas);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async pegaUmaPessoa(req, res) {
+    const { id } = req.params;
+    try {
+      const umaPessoa = await database.Pessoas.findOne({
+        where: {
+          id: Number(id)
+        },
+      });
+      return res.status(200).json(umaPessoa);
     } catch (error) {
       return res.status(500).json(error.message);
     }
